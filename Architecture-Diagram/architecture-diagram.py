aa375759b49d1bd111954949b129dab8008d95cc
dailycarshinewash <dailycarshinewash@gmail.com>
2023-10-08 20:32:22 +0530

    update infra

--- Architecture-Diagram/architecture-diagram.py
+++ Architecture-Diagram/architecture-diagram.py
@@ -1,14 +1,14 @@
 import argparse, sys, os
 from diagrams import Cluster, Diagram, Edge
-from diagrams.aws.compute import ECS, Fargate, EC2, AutoScaling, ECR, ElasticContainerServiceContainer 
+from diagrams.aws.compute import ECS, Fargate, AutoScaling, ECR, ElasticContainerServiceContainer, ElasticContainerServiceService 
 from diagrams.aws.network import (
     VPC,
     Route53,
+    CF,
     PrivateSubnet,
     PublicSubnet,
     InternetGateway,
-    ALB,
-    NATGateway
+    ALB
 )
 from diagrams.aws.security import WAF
 from diagrams.onprem.compute import Server
@@ -22,8 +22,7 @@ filename = "nginxdemo"
 filenamegraph = "nginxdemo.gv"
 show = False
 direction = "LR"
-smaller = "0.8"
-
+smaller = "1"
 
 with Diagram(
     name=title,
@@ -35,40 +34,47 @@ with Diagram(
     # Non Clustered
     waf = WAF("waf")
     user = Server("user")
-
     dns = Route53("dns")
-    
+    cname = Route53("cname")
+    cf = CF("Cloudfront CDN")
     nginx_ecr = ECR("nginxdemo-ecr")
     # Cluster = Group, so this outline will group all the items nested in it automatically
-    with Cluster("vpc"):
-        vpc = VPC("nginx-demo-vpc")
-        igw_gateway = InternetGateway("igw")  
-        lb = ALB("App load balancer")   
+    with Cluster("AWS Region"):
+        with Cluster("nginx-demo-vpc"):
+            vpc = VPC("nginx-demo-vpc")
+            igw_gateway = InternetGateway("igw")  
+            lb = ALB("App load balancer")   
+            
+            with Cluster("ECS"):
+                nginx_ecs = ECS("nginx_ecs")
+                nginx_ecs_service = ElasticContainerServiceService("ecs_service")
+                with Cluster("ecs_autoscaling"):
+                    nginx_ecs_autoscaling = AutoScaling("nginx_ecs_autoscaling")
+                    with Cluster("az1"):
+                        with Cluster("Farget"):
+                            fargate_1 = Fargate("Farget_az1")
+                            taskaz1 = ElasticContainerServiceContainer("tasks")
+                
+                    with Cluster("az2"):            
+                        with Cluster("Farget"):
+                            fargate_2 = Fargate("Farget_az2")
+                            taskaz2 = ElasticContainerServiceContainer("tasks")
+
+                    with Cluster("az3"):            
+                        with Cluster("Farget"):
+                            fargate_3 = Fargate("Farget_az3")
+                            taskaz3 = ElasticContainerServiceContainer("tasks")
         
-        with Cluster("az1"):
-            with Cluster("Farget"):
-                fargate_1 = Fargate("Farget_az1")
-                taskaz1 = ElasticContainerServiceContainer("tasks")
-       
-        with Cluster("az2"):            
-            with Cluster("Farget"):
-                fargate_2 = Fargate("Farget_az2")
-                taskaz1 = ElasticContainerServiceContainer("tasks")
-
-        with Cluster("az3"):            
-            with Cluster("Farget"):
-                fargate_3 = Fargate("Farget_az3")
-                taskaz1 = ElasticContainerServiceContainer("tasks")
-       
     fargate_group = [fargate_1, fargate_2, fargate_3]
-
+    tasks_group = [taskaz1, taskaz2, taskaz3]
     # Now I document the flow here for clarity
     # Could do it in each node area, but I like the "connection flow" to be at the bottom
     ###################################################
     # FLOW OF ACTION, NETWORK, or OTHER PATH TO CHART #
     ################################################### 
-    user >> dns >> waf >> igw_gateway >> lb
+    waf >> cf
+    user >> dns >> cf >> cname>>  lb
     lb - Edge(color="black", style="bold") >> fargate_group
-    nginx_ecr - Edge(color="brown", style="dotted") >> fargate_group
-
+    nginx_ecr - Edge(color="brown", style="normal") >> fargate_group
+    nginx_ecs_service - Edge(color="blue", style="bold") >> fargate_group
 diag
